d00a6f121c6db63b6fa09e5af6d9253ab995f822
Akash Peri <akash.peri@gmail.com>
2014-08-20 06:48:55 -0700

    Icarus - Deus Ex

---
+++ src2/test2.c
@@ -0,0 +1,12 @@
+/*
+ * test2.cpp
+ *
+ *  Created on: Aug 19, 2014
+ *      Author: aperi
+ */
+
+#include "test2.h"
+#include "test.c"
+
+
+
---
+++ src2/test2.h
@@ -0,0 +1,9 @@
+/*
+ * test2.h
+ *
+ *  Created on: Aug 19, 2014
+ *      Author: aperi
+ */
+
+#define TRUE	1
+#define FALSE	0
